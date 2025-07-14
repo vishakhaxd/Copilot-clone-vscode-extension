ee0e5153bdb2906d19d1e496138676fac7ecf3b2
anshul <anshulmaurya27@gmail.com>
2025-07-13 21:00:39 -0400

    add: run and submit commands to support native buttons

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,7 @@
 import * as vscode from 'vscode';
 import { ChatViewProvider } from './chatViewProvider';
+import { exec } from 'child_process';
+import { getWorkspaceInfo } from './util';
 
 export function activate(context: vscode.ExtensionContext) {
     // Set context for when chat is enabled
@@ -22,7 +24,55 @@ export function activate(context: vscode.ExtensionContext) {
         vscode.window.showInformationMessage('Chat history cleared!');
     });
 
-    context.subscriptions.push(openChatCommand, clearChatCommand);
+    // Register command to run code
+    const runCodeCommand = vscode.commands.registerCommand('dsp-cipher.run', () => {
+        const message = vscode.window.setStatusBarMessage('$(sync~spin) Runnind codebase ...');
+        try {   
+                const workspaceInfo = getWorkspaceInfo();
+                const cmd = `/bin/req ${workspaceInfo?.name} run`;
+                exec(cmd, (error, stdout, stderr) => {
+                    if (error) {
+                        vscode.window.showErrorMessage(`Run Error`);
+                        return;
+                    }
+                    if (stderr) {
+                        return;
+                    }
+                });
+            } catch (err) {
+                vscode.window.showErrorMessage(`Run failed try again`);
+            }
+        // extra timeout to compensate for dspcoder-panel
+        setTimeout(() => {
+            message.dispose();
+        }, 3000);
+    });
+
+    // Register command to submit code
+    const submitCodeCommand = vscode.commands.registerCommand('dsp-cipher.submit', () => {
+        const message = vscode.window.setStatusBarMessage('$(sync~spin) Submitting codebase ...');
+        try {   
+                const workspaceInfo = getWorkspaceInfo();
+                const cmd = `/bin/req ${workspaceInfo?.name} submit`;
+                exec(cmd, (error, stdout, stderr) => {
+                    if (error) {
+                        vscode.window.showErrorMessage(`submit Error`);
+                        return;
+                    }
+                    if (stderr) {
+                        return;
+                    }
+                });
+            } catch (err) {
+                vscode.window.showErrorMessage(`submit failed try again`);
+            }
+        // extra timeout to compensate for dspcoder-panel
+        setTimeout(() => {
+            message.dispose();
+        }, 3000);
+    });
+
+    context.subscriptions.push(openChatCommand, clearChatCommand, runCodeCommand, submitCodeCommand);
 }
 
 export function deactivate() {
